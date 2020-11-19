9c3c1761f4b34c0883eb78ea852952d23b043cb3
gfxwalker <65649150+gfxwalker@users.noreply.github.com>
2020-11-19 11:03:53 +0900

    x64build/pdboutput/runtimedll

--- Release/build_msvc2017.bat
+++ Release/build_msvc2017.bat
@@ -1,5 +1,13 @@
-mkdir build_msvc2017
-cd build_msvc2017
-cmake -G "Visual Studio 15 2017" -DCMAKE_INSTALL_PREFIX=../install_msvc2017 -DUSE_LLGI=ON -DBUILD_DX12=ON .. -DCMAKE_DEBUG_POSTFIX=d
-cmake --build . --config Debug --target INSTALL
+mkdir build_msvc2017(x64)
+cd build_msvc2017(x64)
+cmake -G "Visual Studio 15 2017" -DCMAKE_GENERATOR_PLATFORM=x64 -DCMAKE_INSTALL_PREFIX=../install_msvc2017(x64) -DUSE_LLGI=ON -DBUILD_DX12=ON .. -DCMAKE_DEBUG_POSTFIX=.Debug -DCMAKE_RELEASE_POSTFIX=.Release -DCMAKE_COMPILE_PDB_OUTPUT_DIRECTORY=../../../install_msvc2017(x64) -DUSE_MSVC_RUNTIME_LIBRARY_DLL=ture
 cmake --build . --config Release --target INSTALL
+cmake --build . --config Debug --target INSTALL
+
+cd ../
+mkdir build_msvc2017(x86)
+cd build_msvc2017(x86)
+cmake -G "Visual Studio 15 2017" -DCMAKE_INSTALL_PREFIX=../install_msvc2017(x86) -DUSE_LLGI=ON -DBUILD_DX12=ON .. -DCMAKE_DEBUG_POSTFIX=.Debug -DCMAKE_RELEASE_POSTFIX=.Release  -DCMAKE_COMPILE_PDB_OUTPUT_DIRECTORY=../../../install_msvc2017(x86) -DUSE_MSVC_RUNTIME_LIBRARY_DLL=ture
+cmake --build . --config Release --target INSTALL
+cmake --build . --config Debug --target INSTALL
+
